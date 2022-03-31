a11714bf188c4b0d2cfd8d260b2794bf1cdb710d
Kenoly <kelechi.a@spinel.consulting>
2022-03-31 14:31:22 +0100

    Added Spring boot starter artifacts

---
+++ kelechi_oze/src/main/java/com/oze/kelechi_oze/KelechiOzeApplication.java
@@ -0,0 +1,13 @@
+package com.oze.kelechi_oze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelechiOzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelechiOzeApplication.class, args);
+	}
+
+}
---
+++ kelechi_oze/src/test/java/com/oze/kelechi_oze/KelechiOzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oze.kelechi_oze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelechiOzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
